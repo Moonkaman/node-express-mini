998b1d0ddaa23384cc553ab4bc2c440ae18b2639
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-11 14:31:41 -0700

    Add get request to /api/users/:id

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ server.post('/api/users', (req, res) => {
   }
 })
 
+server.get('/api/users/:id', (req, res) => {
+  const userID = req.params.id;
+  db.findById(userID)
+    .then(user => user ? res.status(200).json(user) : res.status(404).json({error: 'User not found'}))
+    .catch(_ => res.status(500).json({error: 'The user information could not be retrieved'}));
+})
+
 server.listen(4000, _ => {
   console.log('Server running on port 4000');
 })
