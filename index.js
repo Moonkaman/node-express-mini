37b3f8369720970accdfc6b545e6c9071e3bb5a8
Moonkaman <nickbatmanglidj@gmail.com>
2019-02-11 14:01:31 -0700

    Create get request for /ap/users

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ const db = require('./data/db');
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/users', (req, res) => {
+  db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(500).json({error: 'The users information could not be retrieved'}));
+})
+
+server.listen(4000, _ => {
+  console.log('Server running on port 4000');
+})
